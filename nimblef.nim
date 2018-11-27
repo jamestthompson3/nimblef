d1db87502f08c60e9ee05eca237f8f0de7d00020
Taylor Thompson <jamestthompson3@gmail.com>
2018-11-27 12:41:49 +0200

    Adds support for args

--- nimblef.nim
+++ nimblef.nim
@@ -1,4 +1,10 @@
-import os, strutils, sequtils
+import os, strutils, sequtils, parseopt, re
+
+type cliArg = tuple[kind: CmdLineKind, key: TaintedString, val: TaintedString]
+
+let searchTerm = filter(toSeq(getopt()), proc(i: cliArg): bool = return i.kind == cmdArgument)
+
+let flags = map(filter(toSeq(getopt()), proc(i:cliArg): bool = return i.kind == cmdShortOption), proc(i: cliArg): TaintedString = i.key)
 
 proc parseGitIgnore(openedFile: seq[string]): seq[string] =
   let parsedFiles = filter(openedFile, proc (i: string): bool =  return contains(i, "#") != true and i != "")
@@ -13,7 +19,12 @@ proc listFiles(dir: string, ignored: seq[string]) =
     if any(parsed, proc (i: string): bool = return contains(pathString, i)) or contains(pathString, ".git"):
       continue
     elif kind == pcFile:
-      echo pathString
+     if len(searchTerm) != 0 and searchTerm[0].key != "":
+        if contains(pathString, re(searchTerm[0].key,
+          if any(flags, proc(i: string): bool = return contains(i, "i")): {reIgnoreCase} else: {reStudy})): echo pathString
+        else: continue
+     else:
+        echo pathString
     else:
       listFiles(path, ignored)
 
