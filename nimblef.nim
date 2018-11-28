bcfaa7584d6e51fa7c0ea81edb9f51643c6fc0dd
Taylor Thompson <jamestthompson3@gmail.com>
2018-11-28 14:35:45 +0200

    Improves formatting

--- nimblef.nim
+++ nimblef.nim
@@ -7,19 +7,28 @@ let currDir: string = getCurrentDir()
 let argsSeq: seq[cliArg] = toSeq(getopt())
 
 proc getKeys(targetGroup: CmdLineKind): seq[TaintedString] =
-  map(filter(argsSeq, proc(i:cliArg): bool = return i.kind == targetGroup), proc(i: cliArg): TaintedString = i.key)
+  map(
+    filter(argsSeq,
+      proc(i: cliArg): bool = return i.kind == targetGroup),
+        proc(i: cliArg): TaintedString = i.key)
 
-let searchTerm = filter(argsSeq, proc(i: cliArg): bool = return i.kind == cmdArgument)
+let searchTerm = filter(argsSeq,
+  proc(i: cliArg): bool = return i.kind == cmdArgument)
+
 let flags = getKeys(cmdShortOption)
 let opts = getKeys(cmdLongOption)
 
 let searchTermIsNotEmpty = len(searchTerm) != 0 and searchTerm[0].key != ""
-let searchIsCaseSensitive = any(flags, proc(i: string): bool = return contains(i, "s"))
-let gitIgnoreIsNotUsed = any(opts, proc(i: string): bool = return contains(i, "no-ignore"))
+let searchIsCaseSensitive = any(flags,
+  proc(i: string): bool = return contains(i, "s"))
+
+let gitIgnoreIsNotUsed = any(opts,
+proc(i: string): bool = return contains(i, "no-ignore"))
 
 
 proc parseGitIgnore(openedFile: seq[string]): seq[string] =
-  let parsedFiles = filter(openedFile, proc (i: string): bool =  return contains(i, "#") != true and i != "")
+  let parsedFiles = filter(openedFile,
+    proc (i: string): bool =  return contains(i, "#") != true and i != "")
   return parsedFiles
 
 proc listFiles(dir: string, ignored: seq[string]) =
@@ -27,11 +36,13 @@ proc listFiles(dir: string, ignored: seq[string]) =
   for kind, path in walkDir(dir):
     var pathString: string = replace(path, currDir)
     pathString.removePrefix({ '/', '\\' })
-    if any(parsed, proc (i: string): bool = return contains(pathString, i)) or contains(pathString, ".git"):
+    if any(parsed, proc (i: string): bool =
+        return contains(pathString, i)) or contains(pathString, ".git"):
       continue
     elif kind == pcFile:
      if searchTermIsNotEmpty:
-        if contains(pathString, re(searchTerm[0].key, {if searchIsCaseSensitive: reStudy else: reIgnoreCase})):
+        if contains(pathString, re(searchTerm[0].key,
+          {if searchIsCaseSensitive: reStudy else: reIgnoreCase})):
           echo pathString
         else:
           continue
@@ -41,9 +52,11 @@ proc listFiles(dir: string, ignored: seq[string]) =
       listFiles(path, ignored)
 
 var f: File
+
 if open(f,".gitignore") and gitIgnoreIsNotUsed != true:
   let ignored: seq[string] = toSeq(lines(f))
   listFiles(currDir, ignored)
+  close(f)
 else:
   listFiles(currDir, newSeqWith(0, ""))
 
