05e6a93ff4b06f7217310dd478f4be99edc7aab1
Taylor Thompson <jamestthompson3@gmail.com>
2019-01-14 22:16:40 +0200

    using coroutines and queues

--- utils/streamBuilder.nim
+++ utils/streamBuilder.nim
@@ -1,87 +1,78 @@
-import terminal, strutils, re, os, threadpool
-
-{.experimental.}
-
-type
-  StreamStatus {.pure.} = enum
-    Buffering, Streaming
-
-const MAX_BUFFER_TIME = 50
+import terminal, strutils, re, os, queues, coro
 
 template colorEcho*(s: string, fg: ForegroundColor) =
   setForeGroundColor(fg, true)
   s.writeStyled({})
   resetAttributes()
 
-proc echoColor(msg: string) {.thread.} =
+proc echoColor(msg: string) =
   for i in split(msg, re"\/|\\"):
     if match(i, re"[a-zA-Z]*\.[a-zA-Z]+"):
       colorEcho(replace(msg, i), fgBlue)
       colorEcho(i, fgWhite)
       echo ""
 
-proc echoFiles(channel: ptr Channel[string], colors: bool = false) {.thread.} =
-  var mode = StreamStatus.Buffering
-  while true:
-    if mode == StreamStatus.Buffering:
-      let messages = channel[].peek()
-      for message in 0..messages:
-        let (_, msg) = channel[].tryRecv()
-        if msg != "":
-          mode = StreamStatus.Streaming
-          if colors:
-            echoColor(msg)
-          else:
-            echo msg
-      mode = StreamStatus.Buffering
-
-proc searchFiles(dir: string, searchTerm: string,
-  caseSensitive: bool, hidden: bool, parsed: seq[string],
-  rootDir: string, channel: ptr Channel[string]
-  ) {.thread.} =
-
-  for kind, path in walkDir(dir):
-    if parsed.anyIt(contains(path, it)) or path.contains(".git"):
-      continue
-
-    var pathString: string = replace(path, rootDir)
-    pathString.removePrefix({ '/', '\\' })
-    if not hidden and startsWith(pathString, "."):
-      continue
-
-    if kind == pcFile:
-      if len(searchTerm) == 0 or pathString.contains(re(searchTerm,
-        {if caseSensitive: reStudy else: reIgnoreCase})):
-          channel[].send(pathString)
-
-    else:
-      searchFiles(
-        pathString,
-        searchTerm,
-        caseSensitive,
-        hidden,
-        parsed,
-        rootDir,
-        channel
-        )
-
+# proc echoFiles(fileQueue: Queue[string], colors: bool = false) {.thread.} =
+ # while fileQueue.len > 0:
+ #   echo fileQueue.pop()
+ # for message in items(fileQueue):
+ #   if message != "":
+ #     if colors:
+ #       echoColor(pop(fileQueue))
+ #     else:
+ #       echo pop(fileQueue)
 
 proc buildStream*(dir: string, colors: bool, searchTerm: string,
   caseSensitive: bool, hidden: bool, parsed: seq[string],
   rootDir: string
   ) =
-  var channel: Channel[string]
+  var coro1: CoroutineRef
+  var fileQueue = initQueue[string]()
 
-  channel.open()
+  proc searchFiles(dir: string, searchTerm: string,
+    caseSensitive: bool, hidden: bool, parsed: seq[string],
+    rootDir: string
+    ) {.thread.} =
 
-  spawn echoFiles(addr(channel), colors)
-  searchFiles(
+    for kind, path in walkDir(dir):
+      if parsed.anyIt(contains(path, it)) or path.contains(".git"):
+        continue
+
+      var pathString: string = replace(path, rootDir)
+      pathString.removePrefix({ '/', '\\' })
+      if not hidden and startsWith(pathString, "."):
+        continue
+
+      if kind == pcFile:
+        if len(searchTerm) == 0 or pathString.contains(re(searchTerm,
+          {if caseSensitive: reStudy else: reIgnoreCase})):
+            fileQueue.add(pathString)
+
+      else:
+        coro.wait(coro.start(
+          proc() =
+            while fileQueue.len > 0:
+              if colors:
+                echoColor(fileQueue.pop())
+              else:
+                echo fileQueue.pop()
+          ))
+        searchFiles(
+          pathString,
+          searchTerm,
+          caseSensitive,
+          hidden,
+          parsed,
+          rootDir
+          )
+
+  coro.start(proc() = searchFiles(
     dir,
     searchTerm,
     caseSensitive,
     hidden,
     parsed,
     rootDir,
-    addr(channel)
-    )
+    ))
+  coro.run()
 
